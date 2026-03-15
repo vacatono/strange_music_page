068127769cf8076c2bc23f43ad84eb901b150ef8
kei sugawara <vacatono@gmail.com>
2026-03-15 13:40:40 +0900

    @astrojs/sitemapのインストール

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vacatono.github.io',
   base: '/strange_music_page',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
