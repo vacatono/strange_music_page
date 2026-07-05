55df9fe48b92a87194de199c6675286f2c43e4e2
kei sugawara <vacatono@gmail.com>
2026-07-05 21:49:25 +0900

    astroでのogp画像処理デバッグ

--- site/src/content.config.ts
+++ site/src/content.config.ts
@@ -3,14 +3,14 @@ import { glob } from 'astro/loaders';
 
 const livereports = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/livereports" }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     date: z.coerce.date(),
     source: z.string().optional(),
     venue: z.string().optional(),
     artists: z.array(z.string()).optional(),
     setlist: z.array(z.string()).optional(),
-    image: z.string().optional(),
+    image: image().optional(),
   }),
 });
 
